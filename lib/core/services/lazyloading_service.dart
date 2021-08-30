de29ae371020030590dc79339a4baa4dc7bd71be
selvakumar.t <selva.t@tamanna.com>
2021-08-30 22:43:35 +0400

    lazyloading method 2 - very easy approach

--- lib/core/services/lazyloading_service.dart
+++ lib/core/services/lazyloading_service.dart
@@ -1,24 +1,14 @@
-import 'dart:math';
-
 import 'package:english_words/english_words.dart';
 
 class ItemFetcher {
-  final _count = 3;
-  final _itemsPerPage = 5;
-  int _currentPage = 0;
-
-  // This async function simulates fetching results from Internet, etc.
+  final rowCount = 10;
   Future<List<WordPair>> fetch() async {
     final list = <WordPair>[];
-    final n = min(_itemsPerPage, _count - _currentPage * _itemsPerPage);
-    // Uncomment the following line to see in real time now items are loaded lazily.
-    // print('Now on page $_currentPage');
-    await Future.delayed(Duration(seconds: 1), () {
-      for (int i = 0; i < n; i++) {
+    await Future.delayed(Duration(seconds: 5), () {
+      for (int i = 0; i < rowCount; i++) {
         list.add(WordPair.random());
       }
     });
-    _currentPage++;
     return list;
   }
 }
--- lib/ui/views/lazyloading_view.dart
+++ lib/ui/views/lazyloading_view.dart
@@ -1,4 +1,5 @@
 import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:learncard/core/services/lazyloading_service.dart';
 
@@ -10,67 +11,46 @@ class LazyLoadingView extends StatefulWidget {
 }
 
 class _LazyLoadingViewState extends State<LazyLoadingView> {
-  final _pairList = <WordPair>[];
-  final _biggerFont = TextStyle(fontSize: 18.0);
-  final _itemFetcher = ItemFetcher();
-  bool _isLoading = true;
-  bool _hasMore = true;
+  List _pairList = <WordPair>[];
+  ItemFetcher _itemFetcher = ItemFetcher();
+  ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    _isLoading = true;
-    _hasMore = true;
     _loadMore();
-  }
-
-  // Triggers fecth() and then add new items or change _hasMore flag
-  void _loadMore() {
-    _isLoading = true;
-    _itemFetcher.fetch().then((List<WordPair> fetchedList) {
-      if (fetchedList.isEmpty) {
-        setState(() {
-          _isLoading = false;
-          _hasMore = false;
-        });
-      } else {
-        setState(() {
-          _isLoading = false;
-          _pairList.addAll(fetchedList);
-        });
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _loadMore();
       }
     });
   }
 
+  void _loadMore() {
+    _itemFetcher.fetch().then((List<WordPair> fetchedList) {
+      _pairList.addAll(fetchedList);
+      setState(() {});
+    });
+  }
+
   Widget build(BuildContext context) {
-    print('reloading.........1');
     return Scaffold(
       appBar: AppBar(
         title: Text('Lazy Loading'),
       ),
       body: Container(
         child: ListView.builder(
-          // Need to display a loading tile if more items are coming
-          itemCount: _hasMore ? _pairList.length + 1 : _pairList.length,
-          itemBuilder: (BuildContext context, int index) {
-            // Uncomment the following line to see in real time how ListView.builder works
-            // print('ListView.builder is building index $index');
-            if (index >= _pairList.length) {
-              // Don't trigger if one async loading is already under way
-              if (!_isLoading) {
-                _loadMore();
-              }
-              return Center(
-                child: SizedBox(
-                  child: CircularProgressIndicator(),
-                  height: 24,
-                  width: 24,
-                ),
-              );
+          controller: _scrollController,
+          itemExtent: 80,
+          itemCount: _pairList.length + 1,
+          itemBuilder: (context, index) {
+            if (index == _pairList.length) {
+              return CupertinoActivityIndicator();
             }
             return ListTile(
-              leading: Text(index.toString(), style: _biggerFont),
-              title: Text(_pairList[index].asPascalCase, style: _biggerFont),
+              leading: Text(index.toString()),
+              title: Text(_pairList[index].asPascalCase),
             );
           },
         ),
